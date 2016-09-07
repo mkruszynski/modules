9e8472281414679295e11667fcfb36c83aa9b742
mkruszynski <mkruszynski@soldevelo.com>
2016-09-07 13:16:24 +0200

    MOTECH-2895: 3rd try

--- config_services.sh
+++ config_services.sh
@@ -7,3 +7,5 @@ sudo apt-get install -y activemq -qq
 sudo ln -s /etc/activemq/instances-available/main /etc/activemq/instances-enabled/main
 sudo sed -e 's/<broker /<broker schedulerSupport="true" /' -i /etc/activemq/instances-enabled/main/activemq.xml
 sudo service activemq restart
+
+sudo chmod +r ./testdata/bootstrap.properties
