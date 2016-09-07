6e7e370eedd39e590ba4ac69aca4398dec3b7967
mkruszynski <mkruszynski@soldevelo.com>
2016-09-07 15:30:32 +0200

    MOTECH-2895: 16th try

--- run_build.sh
+++ run_build.sh
@@ -4,12 +4,13 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     if [ "$DB" = "mysql" ]; then
         mv ./testdata/mysql/bootstrap.properties .
         ls
+        ls ../../
+        ls $HOME
         sudo chmod +r ./bootstrap.properties
         echo "USE mysql;\nUPDATE user SET password=PASSWORD('password') WHERE user='root';\nFLUSH PRIVILEGES;\n" | mysql -u root
         mvn clean install -PIT -U
     elif [ "$DB" = "psql" ]; then
         mv ./testdata/psql/bootstrap.properties .
-        ll
         chmod +r ./bootstrap.properties
         mvn -Dmotech.sql.password=password -Dmotech.sql.user=postgres -Dmaven.test.failure.ignore=false -Dmotech.sql.driver=org.postgresql.Driver -Dmotech.sql.dbtype=psql -Dmotech.sql.url=jdbc:postgresql://localhost:5432/ clean install -PIT -U
     fi
