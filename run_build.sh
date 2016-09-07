4815e039fb47e3cdbdaccafd21b86ee3bfbd2e4b
mkruszynski <mkruszynski@soldevelo.com>
2016-09-07 15:09:19 +0200

    MOTECH-2895: 13th try

--- run_build.sh
+++ run_build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-    ./tx/generate-config.sh
     if [ "$DB" = "mysql" ]; then
         echo "USE mysql;\nUPDATE user SET password=PASSWORD('password') WHERE user='root';\nFLUSH PRIVILEGES;\n" | mysql -u root
         mvn clean install -PIT -U
