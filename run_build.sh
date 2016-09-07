a24f68ca8acb1f6e23bcf8d8e4ee2117b84902fd
mkruszynski <mkruszynski@soldevelo.com>
2016-09-07 15:45:17 +0200

    MOTECH-2895: 19th try

--- run_build.sh
+++ run_build.sh
@@ -2,25 +2,13 @@
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     if [ "$DB" = "mysql" ]; then
-        mv ./testdata/mysql/bootstrap.properties .
-        echo "Tutej"
-        ls
-        echo "1 wstecz"
-        ls ../
-        echo "2 wstecz"
-        ls ../../
-        echo "3 wstecz"
-        ls ../../../
-        echo "$ wstecz"
-        ls ../../../../
-        echo "dom"
-        ls $HOME
-        sudo chmod +r ./bootstrap.properties
+        mv ./testdata/mysql/bootstrap.properties ./testdata/
+        sudo chmod +r ./testdata/bootstrap.properties
         echo "USE mysql;\nUPDATE user SET password=PASSWORD('password') WHERE user='root';\nFLUSH PRIVILEGES;\n" | mysql -u root
         mvn clean install -PIT -U
     elif [ "$DB" = "psql" ]; then
-        mv ./testdata/psql/bootstrap.properties .
-        chmod +r ./bootstrap.properties
+        mv ./testdata/psql/bootstrap.properties ./testdata/
+        chmod +r ./testdata/bootstrap.properties
         mvn -Dmotech.sql.password=password -Dmotech.sql.user=postgres -Dmaven.test.failure.ignore=false -Dmotech.sql.driver=org.postgresql.Driver -Dmotech.sql.dbtype=psql -Dmotech.sql.url=jdbc:postgresql://localhost:5432/ clean install -PIT -U
     fi
 fi
