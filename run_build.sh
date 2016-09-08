8bb7a65fdc8ff130b40b66eb1b1873bfdc34cfcd
mkruszynski <mkruszynski@soldevelo.com>
2016-09-08 13:30:07 +0200

    MOTECH-2895: 51th try

--- run_build.sh
+++ run_build.sh
@@ -3,7 +3,7 @@
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     if [ "$DB" = "mysql" ]; then
         echo "USE mysql;\nUPDATE user SET password=PASSWORD('password') WHERE user='root';\nFLUSH PRIVILEGES\n" | mysql -u root
-        export MOTECH_CONFIG_DIR=`pwd`/testdata/mysql/
+        chmod -R a+r ~/.motech/
         mvn clean install -PIT -U
     elif [ "$DB" = "psql" ]; then
         cp ./testdata/psql/bootstrap.properties .
