97cfe654322ba29817b660db03588813beae515c
mkruszynski <mkruszynski@soldevelo.com>
2016-09-09 15:39:24 +0200

    MOTECH-2895: 103rd try

--- run_build.sh
+++ run_build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-    #export MOTECH_CONFIG_DIR=~/.motech/config/
+    cp ./testdata/config-locations.properties ~/.motech/
     if [ "$DB" = "mysql" ]; then
         echo "USE mysql;\nUPDATE user SET password=PASSWORD('password') WHERE user='root';\nFLUSH PRIVILEGES\n" | mysql -u root
         mvn -Dmysql.password=password -Dmaven.test.failure.ignore=false  clean install -Dmysql.user=root -Dmotech.sql.driver=com.mysql.jdbc.Driver -Dmotech.sql.dbtype=mysql -Dmotech.sql.url=jdbc:mysql://localhost:3306/ clean install -PIT -U
