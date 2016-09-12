fc5951a580f0a4669383e8fdeb0097b2e8d8e705
mkruszynski <mkruszynski@soldevelo.com>
2016-09-12 08:58:31 +0200

    MOTECH-2895: 111th try

--- run_build.sh
+++ run_build.sh
@@ -3,12 +3,8 @@
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     rm -rf ~/.motech/config
     if [ "$DB" = "mysql" ]; then
-        echo ".motech przed mvn"
-        ls -l ~/.motech
         echo "USE mysql;\nUPDATE user SET password=PASSWORD('password') WHERE user='root';\nFLUSH PRIVILEGES\n" | mysql -u root
-        mvn clean install -Dmaven.repo.local=~/.motech -PIT -U
-        echo ".motech PO"
-        ls -l ~/.motech
+        mvn clean install -PIT -U
     elif [ "$DB" = "psql" ]; then
         mvn -Dmotech.sql.password=password -Dmotech.sql.user=postgres -Dmaven.test.failure.ignore=false -Dmotech.sql.driver=org.postgresql.Driver -Dmotech.sql.dbtype=psql -Dmotech.sql.url=jdbc:postgresql://localhost:5432/ clean install -PIT -U
     fi
