2f85f4fae0fe7ea41f5f44f57a29cea04caa2438
mkruszynski <mkruszynski@soldevelo.com>
2016-09-12 09:29:37 +0200

    MOTECH-2895: 114th try

--- run_build.sh
+++ run_build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    mkdir ~/.motech
+    chmod a+rw ~/.motech
     if [ "$DB" = "mysql" ]; then
         echo "USE mysql;\nUPDATE user SET password=PASSWORD('password') WHERE user='root';\nFLUSH PRIVILEGES\n" | mysql -u root
         mvn clean install -PIT -U
