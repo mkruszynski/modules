c09aec5e804f468e6ef195754afe778157c96587
mkruszynski <mkruszynski@soldevelo.com>
2017-01-18 09:22:40 +0100

    Test 6

--- run_build.sh
+++ run_build.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-mvn --version
-rm -r ~/.m2/repository/org/motechproject/*
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     if [ "$DB" = "psql" ]; then
         mvn -Dmotech.sql.password=password -Dmotech.sql.user=postgres -Dmaven.test.failure.ignore=false -Dmotech.sql.driver=org.postgresql.Driver -Dmotech.sql.dbtype=psql -Dmotech.sql.url=jdbc:postgresql://localhost:5432/ clean install -PIT -U -B
     else
-        mvn clean install -PIT -U -B
+        mvn -DdependencyLocationsEnabled=false clean install -PIT -U -B
     fi
 elif [ "$TRAVIS_BRANCH" = "master" ] && [ "$DB" = "mysql" ]; then
 
